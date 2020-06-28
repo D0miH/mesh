4f5fb23bda94a4adf8f96720363ac3cb535996ce
Dominik <d0mih.github@gmail.com>
2020-06-28 16:21:39 +0200

    added support to load obj files by pyrender

--- mesh/serialization/serialization.py
+++ mesh/serialization/serialization.py
@@ -192,6 +192,7 @@ def write_obj(self, filename, flip_faces=False, group=False, comments=None):
                     write_face_to_obj_file(face_index, fi)
         else:
             if hasattr(self, 'f'):
+                fi.write("usemtl {}\n".format(outbase))
                 for face_index in range(len(self.f)):
                     write_face_to_obj_file(face_index, fi)
 
